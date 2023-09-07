79a27c905178ea247219ed8af2be79d650fe2c34
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-07 21:29:27 +0530

    Implement app list
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I5b0b9254476e5282a33877e384cfcf11b1b09994

---
+++ app/src/main/java/org/calyxos/datura/utils/CommonUtils.kt
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import androidx.core.graphics.drawable.toBitmap
+import org.calyxos.datura.models.App
+
+object CommonUtils {
+
+    fun getAllPackages(context: Context): List<App> {
+        val applicationList = mutableListOf<App>()
+        val packageManager = context.packageManager
+
+        val packageList = packageManager.getInstalledPackages(
+            PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong())
+        ).filter { it.requestedPermissions.contains(Manifest.permission.INTERNET) }
+
+        packageList.forEach { packageInfo ->
+            val app = App(
+                packageInfo.applicationInfo.loadLabel(packageManager).toString(),
+                packageInfo.packageName,
+                packageInfo.applicationInfo.loadIcon(packageManager).toBitmap(96, 96),
+                packageInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0
+            )
+            applicationList.add(app)
+        }
+        applicationList.sortBy { it.name }
+        return applicationList.toList()
+    }
+}
