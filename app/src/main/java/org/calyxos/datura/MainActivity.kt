60498d015d9fb57eb7ec8a5d924c2fc4b4c10e8b
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-07 14:40:53 +0530

    Initial commit
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ifb8004cca50e64d45a2152b5e2e6ebf9b90b1688

---
+++ app/src/main/java/org/calyxos/datura/MainActivity.kt
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
