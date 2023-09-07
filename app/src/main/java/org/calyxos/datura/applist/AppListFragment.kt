9ce08aaa49c44d9d2944acc67f245f6c33e3c9e6
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-07 21:29:27 +0530

    Implement app list
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I5b0b9254476e5282a33877e384cfcf11b1b09994

--- app/src/main/java/org/calyxos/datura/applist/AppListFragment.kt
+++ app/src/main/java/org/calyxos/datura/applist/AppListFragment.kt
@@ -5,9 +5,33 @@
 
 package org.calyxos.datura.applist
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.calyxos.datura.R
+import org.calyxos.datura.main.MainActivityViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint(Fragment::class)
-class AppListFragment : Hilt_AppListFragment(R.layout.fragment_app_list)
+class AppListFragment : Hilt_AppListFragment(R.layout.fragment_app_list) {
+
+    private val viewModel: MainActivityViewModel by activityViewModels()
+
+    @Inject
+    lateinit var appListRVAdapter: AppListRVAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Recycler View
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.appList.collect { appListRVAdapter.submitList(it) }
+        }
+        view.findViewById<RecyclerView>(R.id.recyclerView).adapter = appListRVAdapter
+    }
+}
