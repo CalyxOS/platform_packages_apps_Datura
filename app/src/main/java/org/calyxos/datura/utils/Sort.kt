59823e2c06e6bd3413d6bcbb3c5ff07ad0919b1e
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-14 23:14:40 +0530

    AppListFragment: Allow users to sort list by name or last usage
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I6ea62efd223d6c33483becf58cdd85e10f107d90

---
+++ app/src/main/java/org/calyxos/datura/utils/Sort.kt
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.utils
+
+enum class Sort {
+    LAST_USED,
+    NAME
+}
